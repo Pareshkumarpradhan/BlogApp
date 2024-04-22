485940f1e835179de46e5eed058ddf6e784f535d
Paresh Kumar Pradhan <pareshkupradhan789@gmail.com>
2024-04-22 10:26:32 +0530

    create login

---
+++ src/components/Select.jsx
@@ -0,0 +1,31 @@
+/* eslint-disable react/prop-types */
+import React, {useId} from 'react'
+
+function Select({
+    options,
+    lable,
+    className,
+    ...props
+}, ref) {
+const id = useId()
+
+  return (
+    <div className='w-full'>
+        {lable && <label htmlFor={id} className=''></label>}
+        <select
+         id={id} 
+         {...props} 
+         ref={ref}
+         className={`px-3 py-2 rounded-lg bg-white text-black outline-nonefocus:bg-gray-50 duration-200 border border-gray-200 w-full ${className}`}
+         >
+          {options?.map((option) => (
+            <option key={option} value={option}>
+              {option}
+            </option>
+          ))}
+        </select>
+    </div>
+  )
+}
+
+export default React.forwardRef(Select)
